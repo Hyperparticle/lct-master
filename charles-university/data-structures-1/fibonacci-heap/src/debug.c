4f2511e080f3483dc9a4ac8d11cf554ec4a35dfe
hyperparticle <hyperparticle@gmail.com>
2017-11-10 18:20:11 +0100

    Add join buffer debug function

--- charles-university/data-structures-1/fibonacci-heap/src/debug.c
+++ charles-university/data-structures-1/fibonacci-heap/src/debug.c
@@ -7,6 +7,23 @@
 #include <string.h>
 #include "debug.h"
 
+void print_join_buffer(struct heap *heap) {
+    if (heap->join_buffer == NULL) {
+        return;
+    }
+
+
+    printf("[[%d]] ", heap->join_buffer_size);
+
+    for (int i = 0; i < heap->join_buffer_size; i++) {
+        if (heap->join_buffer[i] != NULL) {
+            printf("%d (i = %d), ", heap->join_buffer[i]->key, i);
+        }
+    }
+
+    printf("\n");
+}
+
 void print_heap(struct heap *heap) {
     if (heap->root == NULL) {
         return;
@@ -15,7 +32,7 @@ void print_heap(struct heap *heap) {
     struct node *next = heap->root;
     struct node *last = next;
 
-    printf("[%d] ", heap->node_count);
+    printf("[%d] ", heap->root_list_count);
 
     do {
         printf("%d (%d), ", next->key, next->child_count);
@@ -26,13 +43,18 @@ void print_heap(struct heap *heap) {
 }
 
 void check_heap(struct heap *heap) {
+    if (heap->root == NULL) {
+        return;
+    }
+
     memset(heap->join_buffer, 0, heap->join_buffer_size * sizeof(struct node *));
     struct node *next = heap->root;
 
     int count = 0;
     do {
         if (heap->join_buffer[next->child_count] != NULL) {
-            struct node *i = heap->join_buffer[next->child_count];
+            print_heap(heap);
+            struct node *same = heap->join_buffer[next->child_count];
             fprintf(stderr, "Multiple trees of the same order! (%d)\n", next->child_count);
         }
 
@@ -41,7 +63,8 @@ void check_heap(struct heap *heap) {
         count++;
     } while (next != heap->root);
 
-    if (count != heap->node_count) {
-        printf("Node count discrepancy (%d, %d)\n", heap->node_count, count);
+    if (count != heap->root_list_count) {
+        print_heap(heap);
+        printf("Root list count discrepancy (%d, %d)\n", heap->root_list_count, count);
     }
 }
--- charles-university/data-structures-1/fibonacci-heap/src/debug.h
+++ charles-university/data-structures-1/fibonacci-heap/src/debug.h
@@ -8,6 +8,7 @@
 
 #include "heap.h"
 
+void print_join_buffer(struct heap *heap);
 void print_heap(struct heap *heap);
 void check_heap(struct heap *heap);
 

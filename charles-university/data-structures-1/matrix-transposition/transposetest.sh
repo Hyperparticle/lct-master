8bd9f3f7ec155888463c5772adc586a63c6aa3ca
hyperparticle <hyperparticle@gmail.com>
2017-11-21 22:23:06 +0100

    Collect more results

--- charles-university/data-structures-1/matrix-transposition/transposetest.sh
+++ charles-university/data-structures-1/matrix-transposition/transposetest.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Simple Test"
-for k in {54..140}
+for k in {54..142}
 do
     ./matrix ${k} -s
 done
 echo
 
 echo "Standard Test"
-for k in {54..140}
+for k in {54..142}
 do
     ./matrix ${k}
 done
@@ -17,7 +17,7 @@ echo
 echo "Cache Test (Simple)"
 for i in 64,64 64,1024 64,4096 512,512 4096,64; do
     IFS=',' read B C <<< "${i}"
-    for k in {54..100}
+    for k in {54..120}
     do
         result=$(./matrix-print ${k} -s | ./cachesim ${B} ${C} | sed -n 3p | cut -f 2 -d " ")
         echo "${B},${C},${k},${result}"
@@ -28,7 +28,7 @@ echo
 echo "Cache Test (Standard)"
 for i in 64,64 64,1024 64,4096 512,512 4096,64; do
     IFS=',' read B C <<< "${i}"
-    for k in {54..100}
+    for k in {54..120}
     do
         result=$(./matrix-print ${k} | ./cachesim ${B} ${C} | sed -n 3p | cut -f 2 -d " ")
         echo "${B},${C},${k},${result}"

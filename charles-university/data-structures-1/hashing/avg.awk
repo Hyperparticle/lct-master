2f2bae5a8767b252617b645309914406a9652de5
hyperparticle <hyperparticle@gmail.com>
2017-12-16 16:12:34 +0100

    Fix awk average

--- charles-university/data-structures-1/hashing/avg.awk
+++ charles-university/data-structures-1/hashing/avg.awk
@@ -1,2 +1,14 @@
-{ sum1 += $0; sum2 += $2; n++ }
-(NR%25000==0){ if (n > 0) print (sum1 / n)"\t"(sum2 / n); }
+(NF < 2) {
+    print $1
+}
+
+(NF == 2) {
+    sum1 += $0;
+    sum2 += $2;
+    n++;
+}
+
+(NR%10000 == 0) {
+    if (n > 0) print (sum1 / n)"\t"(sum2 / n);
+    sum1 = sum2 = n = 0;
+}
--- charles-university/data-structures-1/hashing/src/main.c
+++ charles-university/data-structures-1/hashing/src/main.c
@@ -7,24 +7,40 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "random-gen.h"
 #include "hash-system.h"
 #include "hash-scheme.h"
 
 #define RANDOM_SEED 42
+#define HASH_SIZE 20
+#define NUM_BLOCKS 4
 
 static void benchmark_random(struct hash_table table);
+static void print_usage();
 
 int main(int argc, char **argv) {
     rng_init(RANDOM_SEED);
 
-    struct hash_system system = tabulation_system(20, 4);
+//    if (argc != 1) {
+//        print_usage();
+//    }
+//
+//    if (strcmp(argv[0], "-n") == 0) {
+//
+//    }
+
+
+    printf("Tabulation-Cuckoo\n");
+    struct hash_system system = tabulation_system(HASH_SIZE, NUM_BLOCKS);
     struct hash_table table = hash_table_init(system, tabulate, tabulation_init);
 
 //    struct hash_system system = multiply_shift_system(20);
 //    struct hash_table table = hash_table_init(system, multiply_shift, multiply_shift_init);
 
     benchmark_random(table);
+
+    printf("\n");
 
     return 0;
 }
@@ -41,4 +57,9 @@ static void benchmark_random(struct hash_table table) {
             printf("%lu\t%f\n", result, alpha);
         }
     }
+}
+
+static void print_usage() {
+    printf("Usage:\n");
+    printf("hashing [-r|-s] [-c|-l] [-t|-m|-n]\n");
 }

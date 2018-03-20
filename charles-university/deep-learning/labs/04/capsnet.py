1c9f065e6be2614cff391697c86961946c7b76f4
hyperparticle <hyperparticle@gmail.com>
2018-03-20 23:15:57 +0100

    Capsnet, now even simpler

--- charles-university/deep-learning/labs/04/capsnet.py
+++ charles-university/deep-learning/labs/04/capsnet.py
@@ -1,12 +1,11 @@
 import numpy as np
 import tensorflow as tf
 import keras
-from keras.preprocessing.image import ImageDataGenerator
-
-keras.backend.set_image_data_format('channels_last')
 
 
 class CapsNet:
+    """Implementation of CapsNet from https://arxiv.org/pdf/1710.09829.pdf"""
+
     def __init__(self, input_shape, n_class, load_weights=False):
         self.model_filename = 'capsnet.h5'
         self.construct(input_shape, n_class)
@@ -15,30 +14,32 @@ class CapsNet:
         if load_weights:
             self.train_model.load_weights(self.model_filename)
 
-    def construct(self, input_shape, n_class):
-        x = keras.layers.Input(shape=input_shape)
+    def construct(self, input_shape, classes):
+        x = keras.layers.Input(input_shape)
 
-        conv = keras.layers.Conv2D(filters=256, kernel_size=9, strides=1, padding='valid',
-                                   activation='relu')(x)
+        conv1 = keras.layers.Conv2D(256, kernel_size=9, strides=1, padding='valid', activation='relu')(x)
+        primary_caps = keras.layers.Conv2D(8 * 32, kernel_size=9, strides=2, padding='valid')(conv1)
+        primary_caps = keras.layers.Reshape([-1, 8])(primary_caps)
+        primary_caps = keras.layers.Lambda(squash)(primary_caps)
 
-        primarycaps = primary_capsule_layer(conv, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
+        caps = CapsuleLayer(classes, 16)(primary_caps)
 
-        digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16)(primarycaps)
+        output_caps = keras.layers.Lambda(
+            lambda inputs: tf.sqrt(tf.reduce_sum(tf.square(inputs), -1)),
+            name='capsnet')(caps)
 
-        out_caps = Length(name='capsnet')(digitcaps)
+        y = keras.layers.Input((classes,))
+        masked_train = Mask()([caps, y])
+        masked = Mask()(caps)
 
-        y = keras.layers.Input(shape=(n_class,))
-        masked_by_y = Mask()([digitcaps, y])
-        masked = Mask()(digitcaps)
-
-        decoder = keras.models.Sequential(name='decoder')
-        decoder.add(keras.layers.Dense(512, activation='relu', input_dim=16*n_class))
+        decoder = keras.models.Sequential()
+        decoder.add(keras.layers.Dense(512, activation='relu', input_dim=16 * classes))
         decoder.add(keras.layers.Dense(1024, activation='relu'))
         decoder.add(keras.layers.Dense(np.prod(input_shape), activation='sigmoid'))
-        decoder.add(keras.layers.Reshape(target_shape=input_shape, name='out_recon'))
+        decoder.add(keras.layers.Reshape(target_shape=input_shape, name='reconstruction'))
 
-        train_model = keras.models.Model([x, y], [out_caps, decoder(masked_by_y)])
-        eval_model = keras.models.Model(x, [out_caps, decoder(masked)])
+        train_model = keras.models.Model([x, y], [output_caps, decoder(masked_train)])
+        eval_model = keras.models.Model(x, [output_caps, decoder(masked)])
 
         self.train_model, self.eval_model = train_model, eval_model
 
@@ -52,31 +53,23 @@ class CapsNet:
                                          histogram_freq=1)
         checkpoint = keras.callbacks.ModelCheckpoint(self.model_filename, monitor='val_capsnet_acc',
                                                      save_best_only=True, save_weights_only=True, verbose=1)
-        lr_decay = keras.callbacks.LearningRateScheduler(schedule=lambda epoch: args.lr * (args.lr_decay ** epoch))
+        learn_rate_decay = keras.callbacks.LearningRateScheduler(
+            schedule=lambda epoch: args.learn_rate * (args.learn_rate_decay ** epoch))
 
-        def margin_loss(y_true, y_pred):
-            L = y_true * tf.square(tf.maximum(0., 0.9 - y_pred)) + \
-                0.5 * (1 - y_true) * tf.square(tf.maximum(0., y_pred - 0.1))
+        margin_loss = lambda true, pred: tf.reduce_mean(tf.reduce_sum(
+            true * tf.square(tf.maximum(0., 0.9 - pred)) +
+            0.5 * (1 - true) * tf.square(tf.maximum(0., pred - 0.1)), 1))
 
-            return tf.reduce_mean(tf.reduce_sum(L, 1))
-
-        model.compile(optimizer=keras.optimizers.Adam(lr=args.lr),
+        model.compile(optimizer=keras.optimizers.Adam(lr=args.learn_rate),
                       loss=[margin_loss, 'mse'],
-                      loss_weights=[1., args.lam_recon],
+                      loss_weights=[1., 0.392],
                       metrics={'capsnet': 'accuracy'})
 
-        def train_generator(x, y, batch_size, shift_fraction=0.):
-            train_datagen = ImageDataGenerator(width_shift_range=shift_fraction, height_shift_range=shift_fraction)
-            generator = train_datagen.flow(x, y, batch_size=batch_size)
-            while True:
-                x_batch, y_batch = generator.next()
-                yield ([x_batch, y_batch], [y_batch, x_batch])
-
-        model.fit_generator(generator=train_generator(x_train, y_train, args.batch_size, args.shift_fraction),
+        model.fit_generator(generator=train_generator(x_train, y_train, args.batch_size, 0.1),
                             steps_per_epoch=int(y_train.shape[0] / args.batch_size),
                             epochs=args.epochs,
                             validation_data=[[x_test, y_test], [y_test, x_test]],
-                            callbacks=[tb, checkpoint, lr_decay])
+                            callbacks=[tb, checkpoint, learn_rate_decay])
 
         model.save_weights(self.model_filename)
 
@@ -96,50 +89,13 @@ class CapsNet:
         return np.argmax(y_pred, 1)
 
 
-class Length(keras.layers.Layer):
-    """Computes the length of the input vectors"""
-    def call(self, inputs, **kwargs):
-        return tf.sqrt(tf.reduce_sum(tf.square(inputs), -1))
-
-    def compute_output_shape(self, input_shape):
-        return input_shape[:-1]
-
-
-class Mask(keras.layers.Layer):
-    """Mask the vectors so that the true label wins out"""
-    def call(self, inputs, **kwargs):
-        if type(inputs) is list:
-            # Mask with true label
-            inputs, mask = inputs
-        else:
-            # Mask with magnitude of output if no true label
-            x = tf.sqrt(tf.reduce_sum(tf.square(inputs), -1))
-            mask = tf.one_hot(indices=tf.argmax(x, 1), depth=x.get_shape().as_list()[1], axis=-1)
-
-        masked = keras.backend.batch_flatten(inputs * tf.expand_dims(mask, -1))
-        return masked
-
-    def compute_output_shape(self, input_shape):
-        if type(input_shape[0]) is tuple:  # true label provided
-            return tuple([None, input_shape[0][1] * input_shape[0][2]])
-        else:  # no true label provided
-            return tuple([None, input_shape[1] * input_shape[2]])
-
-
-def squash(vectors, axis=-1):
-    """Non-linear squash function acting on vectors"""
-    epsilon = 1e-7
-    s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
-    scale = s_squared_norm / (1 + s_squared_norm) / tf.sqrt(s_squared_norm + epsilon)
-    return scale * vectors
-
-
 class CapsuleLayer(keras.layers.Layer):
     """Capsule layer with vector outputs (instead of traditional scalar values)"""
     def __init__(self, num_capsule, dim_capsule,
                  kernel_initializer='glorot_uniform',
                  **kwargs):
         super(CapsuleLayer, self).__init__(**kwargs)
+
         self.num_capsule = num_capsule
         self.dim_capsule = dim_capsule
         self.routings = 3
@@ -153,22 +109,19 @@ class CapsuleLayer(keras.layers.Layer):
                                         self.dim_capsule, self.input_dim_capsule],
                                  initializer=self.kernel_initializer, name='W')
 
-        self.built = True
-
     def call(self, inputs, training=None):
         inputs_expand = tf.expand_dims(inputs, 1)
         inputs_tiled = tf.tile(inputs_expand, [1, self.num_capsule, 1, 1])
-        inputs_hat = tf.map_fn(lambda x: keras.backend.batch_dot(x, self.W, [2, 3]),
-                                          elems=inputs_tiled)
+        inputs_hat = tf.map_fn(lambda x:  tf.keras.backend.batch_dot(x, self.W, [2, 3]), elems=inputs_tiled)
 
         b = tf.zeros(shape=[tf.shape(inputs_hat)[0], self.num_capsule, self.input_num_capsule])
 
         for i in range(self.routings):
             c = tf.nn.softmax(b, axis=1)
-            outputs = squash(keras.backend.batch_dot(c, inputs_hat, [2, 2]))
+            outputs = squash(tf.keras.backend.batch_dot(c, inputs_hat, [2, 2]))
 
             if i < self.routings - 1:
-                b += keras.backend.batch_dot(outputs, inputs_hat, [2, 3])
+                b += tf.keras.backend.batch_dot(outputs, inputs_hat, [2, 3])
 
         return outputs
 
@@ -176,11 +129,37 @@ class CapsuleLayer(keras.layers.Layer):
         return tuple([None, self.num_capsule, self.dim_capsule])
 
 
-def primary_capsule_layer(inputs, dim_capsule, n_channels, kernel_size, strides, padding):
-    """Conv2D layer preceding dynamic routing layer"""
-    output = keras.layers.Conv2D(filters=dim_capsule * n_channels, kernel_size=kernel_size,
-                                 strides=strides, padding=padding,
-                                 name='primarycap_conv2d')(inputs)
-    outputs = keras.layers.Reshape(target_shape=[-1, dim_capsule], name='primarycap_reshape')(output)
+class Mask(keras.layers.Layer):
+    """Mask the vectors so that the true label wins out"""
+    def call(self, inputs, **kwargs):
+        if type(inputs) is list:
+            inputs, mask = inputs
+        else:
+            x = tf.sqrt(tf.reduce_sum(tf.square(inputs), -1))
+            mask = tf.one_hot(tf.argmax(x, 1), x.get_shape().as_list()[1], axis=-1)
 
-    return keras.layers.Lambda(squash, name='primarycap_squash')(outputs)
+        masked = tf.keras.backend.batch_flatten(inputs * tf.expand_dims(mask, -1))
+        return masked
+
+    def compute_output_shape(self, input_shape):
+        if type(input_shape[0]) is tuple:
+            return tuple([None, input_shape[0][1] * input_shape[0][2]])
+        else:
+            return tuple([None, input_shape[1] * input_shape[2]])
+
+
+def squash(vectors, axis=-1):
+    """Non-linear squash function acting on vectors"""
+    epsilon = 1e-7
+    s_squared_norm = tf.reduce_sum(tf.square(vectors), axis, keepdims=True)
+    scale = s_squared_norm / (1 + s_squared_norm) / tf.sqrt(s_squared_norm + epsilon)
+    return scale * vectors
+
+
+def train_generator(x, y, batch_size, shift_fraction=0.):
+    train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+        width_shift_range=shift_fraction, height_shift_range=shift_fraction)
+    generator = train_datagen.flow(x, y, batch_size=batch_size)
+    while True:
+        x_batch, y_batch = generator.next()
+        yield ([x_batch, y_batch], [y_batch, x_batch])
--- charles-university/deep-learning/labs/04/mnist_competition.py
+++ charles-university/deep-learning/labs/04/mnist_competition.py
@@ -31,30 +31,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--batch_size', default=100, type=int)
-    parser.add_argument('--lr', default=0.001, type=float,
-                        help="Initial learning rate")
-    parser.add_argument('--lr_decay', default=0.9, type=float,
-                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
-    parser.add_argument('--lam_recon', default=0.392, type=float,
-                        help="The coefficient for the loss of decoder")
-    parser.add_argument('-r', '--routings', default=3, type=int,
-                        help="Number of iterations used in routing algorithm. should > 0")
-    parser.add_argument('--shift_fraction', default=0.1, type=float,
-                        help="Fraction of pixels to shift at most in each direction.")
+    parser.add_argument('--learn_rate', default=0.001, type=float)
+    parser.add_argument('--learn_rate_decay', default=0.9, type=float)
     parser.add_argument('--load', action='store_true')
     args = parser.parse_args()
 
     (x_train, y_train), (x_val, y_val), x_test = load_mnist()
 
-    model = CapsNet(input_shape=x_train.shape[1:],
-                    n_class=len(np.unique(np.argmax(y_val, 1))),
-                    load_weights=args.load)
+    model = CapsNet(x_train.shape[1:], 10, args.load)
 
     if not args.load:
         model.train(data=((x_train, y_train), (x_val, y_val)), args=args)
 
     accuracy = model.evaluate(data=(x_val, y_val))
-
     predictions = model.predict(x_test)
 
     print(accuracy, '\n')

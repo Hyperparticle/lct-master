224917958bcd7a33c5af312fe26acaf8a48f2ec4
hyperparticle <hyperparticle@gmail.com>
2018-03-27 15:16:28 +0200

    Add smaller dataset

--- charles-university/2018-npfl104/hw/my-dataset/split_data.py
+++ charles-university/2018-npfl104/hw/my-dataset/split_data.py
@@ -2,46 +2,20 @@
 
 import pandas as pd
 import numpy as np
-import pdb
-from sklearn.model_selection import train_test_split
-
-def dump(xydata, split="train"):
-  data_xout = open("x_%s.csv" % split,'w')
-  data_yout = open("y_%s.csv" % split,'w')
-
-  n,m = xydata.shape
-  
-  for i in range(n):
-    try:
-      data_xout.write(",".join([str(x) for x in xydata[i,:-1]] ) + '\n')
-    except:
-      pdb.set_trace()
-    data_yout.write(str(xydata[i,-1]) + '\n')
-
-  data_xout.close()
-  data_yout.close()
-
 
 if __name__ == "__main__":
-  data = pd.read_csv("creditcard.csv")
+  data = pd.read_csv('creditcard.csv')[['V' + str(i+1) for i in range(28)] + ['Class']]
 
-  Y = data["Class"]
-  xc = data[data.columns.difference(["Class"])]
-  X = xc.as_matrix()
-  c1_idx = Y==1
+  true = data[data.Class == 1]
+  false = data[data.Class == 0][:5000 - len(true)]
 
-  xt1,xts1,yt1,yts1 = train_test_split(X[c1_idx,:],Y[c1_idx],test_size=.2,random_state=42)
-  xt2,xts2,yt2,yts2 = train_test_split(X[~c1_idx,:],Y[~c1_idx],test_size=.2,random_state=42)
+  s0, s1 = int(len(true) * 0.8), int(len(false) * 0.8) 
+  true_train, true_test = true[:s0], true[s0:]
+  false_train, false_test = false[:s1], false[s1:]
 
-  xtrain = np.vstack([xt1,xt2])
-  ytrain = np.hstack([yt1,yt2]).reshape([-1,1])
-  xtest = np.vstack([xts1,xts2])
-  ytest = np.hstack([yts1,yts2]).reshape([-1,1])
-
-  xytrain = np.hstack([xtrain,ytrain])
-  xytest = np.hstack([xtest,ytest])
-  np.random.shuffle(xytrain)
-  np.random.shuffle(xytest)
-
-  dump(xytrain,"train")
-  dump(xytest,"test")
+  train = true_train.append(false_train).sample(frac=1).reset_index(drop=True)
+  test = true_test.append(false_test).sample(frac=1).reset_index(drop=True)
+  
+  train.to_csv('train.txt', header=False, index=False)
+  test.to_csv('test.txt', header=False, index=False)
+ 

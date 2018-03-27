cc4e76e48ff285c070f7007064cf062c81b9d10e
hyperparticle <hyperparticle@gmail.com>
2018-03-27 17:11:48 +0200

    Add prague data

--- charles-university/2018-npfl104/hw/my-regression/my-regression.py
+++ charles-university/2018-npfl104/hw/my-regression/my-regression.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction import DictVectorizer
 
 def evaluate(model, train, test):
     model.fit(train[:,:-1], train[:,-1])
@@ -13,8 +14,21 @@ models = [
     [LinearRegression(), 'linear regression'],
 ]
 
-train = np.loadtxt('artificial_2x_train.tsv', delimiter='\t')
-test = np.loadtxt('artificial_2x_test.tsv', delimiter='\t')
+# train = np.loadtxt('artificial_2x_train.tsv', delimiter='\t')
+# test = np.loadtxt('artificial_2x_test.tsv', delimiter='\t')
+# train = np.loadtxt('pragueestateprices_train.tsv', delimiter='\t')
+# test = np.loadtxt('pragueestateprices_test.tsv', delimiter='\t')
 
-for model, name in models:
-    print(name, evaluate(model, train, test))
+
+# for model, name in models:
+#     print(name, evaluate(model, train, test))
+
+v = DictVectorizer(sparse=False)
+train = pd.read_csv('pragueestateprices_train.tsv', delimiter='\t')
+print()
+
+
+
+D = [{'foo': 1, 'bar': 2}, {'foo': 3, 'baz': 1}]
+X = v.fit_transform(D)
+print(X)

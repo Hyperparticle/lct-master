c9fd51f0418e8871ae674ba7f2217bd1caef92ee
hyperparticle <hyperparticle@gmail.com>
2018-03-28 11:58:35 +0200

    Display histogram

--- charles-university/2018-npfl104/hw/my-dataset/visualize.py
+++ charles-university/2018-npfl104/hw/my-dataset/visualize.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 data = pd.read_csv('creditcard.csv')
 
-x,y = data['Amount'], data['Class']
+true = data[data.Class == 1]
+false = data[data.Class == 0]
 
-plt.scatter(x, y)
-plt.xlabel('Amount')
-plt.ylabel('Class')
-# plt.title('V2 as a function of V1')
+x = [true.Amount, false.Amount]
+
+# plt.hist(true.Amount, 100, range=(0, 1000), log=True)
+plt.hist(x, 100, range=(0, 1500), log=True, label=['fraud', 'normal'], color=['tab:red', 'tab:blue'], histtype='bar', stacked=True)
+
+plt.legend(prop={'size': 14})
+
+plt.xlabel('Purchase Amount')
+plt.ylabel('Frequency')
+plt.title('Frequency of Purchases by Value')
 
 plt.savefig('data.png')

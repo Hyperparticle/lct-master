aaaea8bdace13938dcacba2937399caae9cb193f
hyperparticle <hyperparticle@gmail.com>
2018-03-29 00:14:39 +0200

    Improve segmentation accuracy

--- charles-university/deep-learning/labs/05/fashion_masks.py
+++ charles-university/deep-learning/labs/05/fashion_masks.py
@@ -3,7 +3,19 @@ import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 
-from resnet import residual_layer, preprocess
+from resnet import resnet, swish
+
+
+def iou_accuracy(pred_labels, pred_masks, gold_labels, gold_masks):
+    iou = 0
+    for pred_label, pred_mask, gold_label, gold_mask in zip(pred_labels, pred_masks, gold_labels, gold_masks):
+        if pred_label == gold_label:
+            system_mask = np.array(pred_mask, dtype=gold_mask.dtype).reshape(gold_mask.shape)
+            system_pixels = np.sum(system_mask)
+            gold_pixels = np.sum(gold_mask)
+            intersection_pixels = np.sum(system_mask * gold_mask)
+            iou += intersection_pixels / (system_pixels + gold_pixels - intersection_pixels)
+    return 100 * iou / len(gold_labels)
 
 
 class Dataset:
@@ -12,8 +24,6 @@ class Dataset:
         self._images = data["images"]
         self._labels = data["labels"] if "labels" in data else None
         self._masks = data["masks"] if "masks" in data else None
-
-        self._permutation = np.random.permutation(len(self._images))
 
     @property
     def images(self):
@@ -27,16 +37,19 @@ class Dataset:
     def masks(self):
         return self._masks
 
-    def next_batch(self, batch_size):
-        batch_size = min(batch_size, len(self._permutation))
-        batch_perm, self._permutation = self._permutation[:batch_size], self._permutation[batch_size:]
-        return self._images[batch_perm], self._labels[batch_perm], self._masks[batch_perm]
+    def batches(self, batch_size, shift_fraction=0., seed=42):
+        x, y, m = self._images, self._labels, self._masks
 
-    def epoch_finished(self):
-        if len(self._permutation) == 0:
-            self._permutation = np.random.permutation(len(self._images))
-            return True
-        return False
+        train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+            width_shift_range=shift_fraction, height_shift_range=shift_fraction)
+        gen1 = train_datagen.flow(x, y, batch_size=batch_size, seed=seed)
+        gen2 = train_datagen.flow(m, batch_size=batch_size, seed=seed)
+
+        while True:
+            x_batch, y_batch = gen1.next()
+            m_batch = gen2.next()
+
+            yield x_batch, y_batch, m_batch
 
 
 class Network:
@@ -66,50 +79,38 @@ class Network:
             # - mask predictions are stored in `self.masks_predictions` of shape [None, 28, 28, 1] and type tf.float32
             #   with values 0 or 1
 
-            x = preprocess(self.images)
-
-            x = tf.layers.conv2d(x, 16, [3,3], strides=1, padding='SAME', use_bias=False, kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
-            x = tf.layers.batch_normalization(x, training=self.is_training)
-            x = tf.nn.relu(x)
-
-            # [28, 28, 16]
-            for i in range(args.residual_depth):
-                x = residual_layer(x, 16, self.is_training)
-                assert x.shape[1:] == [28, 28, 16]
-
-            # [14, 14, 32]
-            for i in range(args.residual_depth):
-                x = residual_layer(x, 32, self.is_training, downsample=(i==0))
-                assert x.shape[1:] == [14, 14, 32]
-
-            # [7, 7, 64]
-            for i in range(args.residual_depth):
-                x = residual_layer(x, 64, self.is_training, downsample=(i==0))
-                assert x.shape[1:] == [7, 7, 64]
-
-            # Global average pooling
-            x = tf.reduce_mean(x, [1,2])
-            assert x.shape[1:] == [64]
-            # [64]
-
-            output_label = tf.layers.dense(x, self.LABELS, activation=None, name='output_label')
+            # Classification
+            x_label = resnet(self.images, args.residual_depth, self.is_training)
+            output_label = tf.layers.dense(x_label, self.LABELS, name='output_label')
             self.labels_predictions = tf.argmax(output_label, axis=1)
 
-            # x_mask = tf.layers.dense(x, self.WIDTH * self.HEIGHT * 10)
-            # output_mask = tf.layers.dense(x_mask, self.HEIGHT * self.WIDTH, activation=None, name='output_mask')
-            # output_mask = tf.reshape(output_mask, [-1, self.HEIGHT, self.WIDTH, 1])
-            output_mask = tf.zeros_like(self.images)
+            # Segmentation
+            x_mask = resnet(self.images, args.residual_depth, self.is_training)
+            x_mask = tf.layers.dense(x_mask, 512, activation=swish)
+            x_mask = tf.layers.dense(x_mask, 1024, activation=swish)
+            x_mask = tf.layers.dense(x_mask, self.HEIGHT * self.WIDTH, activation=None, name='output_mask')
+            output_mask = tf.reshape(x_mask, [-1, self.HEIGHT, self.WIDTH, 1])
             self.masks_predictions = tf.round(output_mask)
 
             loss_label = tf.losses.sparse_softmax_cross_entropy(self.labels, output_label, scope='loss')
             loss_mask = tf.losses.mean_squared_error(self.masks, output_mask)
 
-            # loss = loss_label + loss_mask
-            loss = loss_label
+            loss = loss_label + loss_mask
             global_step = tf.train.create_global_step()
+
+            # Compute learning rate with decay
+            decay_steps = args.train_size // args.batch_size
+            decay_rate = (args.learning_rate_final / args.learning_rate) ** (1 / (args.epochs - 1))
+            learning_rate = tf.train.exponential_decay(args.learning_rate, global_step, decay_steps, decay_rate,
+                                                       staircase=True)
+
             update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
             with tf.control_dependencies(update_ops):
-                self.training = tf.train.AdamOptimizer().minimize(loss, global_step=global_step, name='training')
+                optimizer = tf.train.AdamOptimizer(learning_rate)
+                gradients, variables = zip(*optimizer.compute_gradients(loss))
+                gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+                self.training = optimizer.apply_gradients(zip(gradients, variables),
+                                                          global_step=global_step, name='training')
 
             # Summaries
             accuracy = tf.reduce_mean(tf.cast(tf.equal(self.labels, self.labels_predictions), tf.float32))
@@ -136,6 +137,9 @@ class Network:
                                                tf.contrib.summary.image(dataset+"/images", self.images),
                                                tf.contrib.summary.image(dataset+"/masks", self.masks_predictions)]
 
+            # Construct the saver
+            self.saver = tf.train.Saver()
+
             # Initialize variables
             self.session.run(tf.global_variables_initializer())
             with summary_writer.as_default():
@@ -146,12 +150,20 @@ class Network:
                          {self.images: images, self.labels: labels, self.masks: masks, self.is_training: True})
 
     def evaluate(self, dataset, images, labels, masks):
-        self.session.run(self.summaries[dataset],
-                         {self.images: images, self.labels: labels, self.masks: masks, self.is_training: False})
+        pred_labels, pred_masks, _ = self.session.run(
+            [self.labels_predictions, self.masks_predictions, self.summaries[dataset]],
+            {self.images: images, self.labels: labels, self.masks: masks, self.is_training: False})
+        return iou_accuracy(pred_labels, pred_masks, labels, masks)
 
     def predict(self, images):
         return self.session.run([self.labels_predictions, self.masks_predictions],
                                 {self.images: images, self.is_training: False})
+
+    def save(self, path):
+        self.saver.save(self.session, path)
+
+    def load(self, path):
+        self.saver.restore(self.session, path)
 
 
 if __name__ == "__main__":
@@ -162,9 +174,12 @@ if __name__ == "__main__":
 
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch_size", default=128, type=int, help="Batch size.")
-    parser.add_argument("--epochs", default=50, type=int, help="Number of epochs.")
+    parser.add_argument("--batch_size", default=64, type=int, help="Batch size.")
+    parser.add_argument("--epochs", default=1000, type=int, help="Number of epochs.")
+    parser.add_argument("--learning_rate", default=0.001)
+    parser.add_argument("--learning_rate_final", default=0.0001)
     parser.add_argument("--residual_depth", default=3, type=int, help="Depth of residual layers.")
+    parser.add_argument("--load", action='store_true')
     args = parser.parse_args()
 
     # Create logdir name
@@ -181,22 +196,38 @@ if __name__ == "__main__":
     dev = Dataset("fashion-masks-dev.npz")
     test = Dataset("fashion-masks-test.npz")
 
+    args.train_size = len(train.images)
+
     # Construct the network
     network = Network()
     network.construct(args)
-    
-    # Train
-    for i in range(args.epochs):
-        print('Epoch', i)
 
-        with tqdm(total=len(train._images)) as pbar:
-            while not train.epoch_finished():
-                images, labels, masks = train.next_batch(args.batch_size)
-                network.train(images, labels, masks)
-                pbar.update(len(images))
-    
-        network.evaluate("dev", dev.images, dev.labels, dev.masks)
-    
+    if not args.load:
+        best_accuracy = 0
+
+        # Train
+        for i in range(args.epochs):
+            print('Epoch', i)
+
+            with tqdm(total=len(train.images)) as pbar:
+                batches = train.batches(args.batch_size)
+                steps_per_epoch = len(train.images)
+                total = 0
+                while total < steps_per_epoch:
+                    images, labels, masks = next(batches)
+                    network.train(images, labels, masks)
+                    pbar.update(len(images))
+                    total += len(images)
+
+            accuracy = network.evaluate("dev", dev.images, dev.labels, dev.masks)
+            print('Val accuracy', accuracy)
+            if accuracy > best_accuracy:
+                network.save('fashion_masks/model')
+
+    network.load('fashion_masks/model')
+    accuracy = network.evaluate("dev", dev.images, dev.labels, dev.masks)
+    print('Final accuracy', accuracy)
+
     labels, masks = network.predict(test.images)
     with open("fashion_masks_test.txt", "w") as test_file:
         for i in range(len(labels)):
--- charles-university/deep-learning/labs/05/resnet.py
+++ charles-university/deep-learning/labs/05/resnet.py
@@ -1,23 +1,89 @@
 import tensorflow as tf
 
+
+def resnet(x, residual_depth, training):
+    x = preprocess(x)
+
+    x = tf.layers.conv2d(x, 16, [3, 3], strides=1, padding='SAME', use_bias=False,
+                         kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
+    x = tf.layers.batch_normalization(x, training=training)
+    x = swish(x)
+
+    for i in range(residual_depth):
+        x = residual_layer(x, 16, training)
+        assert x.shape[1:] == [28, 28, 16]
+
+    for i in range(residual_depth):
+        x = residual_layer(x, 32, training, downsample=(i == 0))
+        assert x.shape[1:] == [14, 14, 32]
+
+    # Classification
+    for i in range(residual_depth):
+        x = residual_layer(x, 64, training, downsample=(i == 0))
+        assert x.shape[1:] == [7, 7, 64]
+
+    # Global average pooling
+    x = tf.reduce_mean(x, [1, 2])
+    assert x.shape[1:] == [64]
+
+    return x
+
+
 def residual_layer(x, output_channels, training, downsample=False):
     stride = 2 if downsample else 1
 
-    input_channels = int(x.shape[-1])
-
     # First hidden layer
-    hidden = tf.layers.conv2d(x, output_channels, [3,3], strides=stride, padding='SAME', use_bias=False, kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
+    hidden = tf.layers.conv2d(x, output_channels, [3, 3], strides=stride, padding='SAME', use_bias=False,
+                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
     hidden = tf.layers.batch_normalization(hidden, training=training)
-    hidden = tf.nn.relu(hidden)
+    hidden = swish(hidden)
 
     # Second hidden layer
-    hidden = tf.layers.conv2d(hidden, output_channels, [3,3], strides=1, padding='SAME', use_bias=False, kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
+    hidden = tf.layers.conv2d(hidden, output_channels, [3, 3], strides=1, padding='SAME', use_bias=False,
+                              kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
     hidden = tf.layers.batch_normalization(hidden, training=training)
 
     if downsample:
-        x = tf.layers.conv2d(x, output_channels, [1,1], strides=stride, padding='SAME', kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
+        x = tf.layers.conv2d(x, output_channels, [1, 1], strides=stride, padding='SAME',
+                             kernel_initializer=tf.contrib.layers.variance_scaling_initializer())
 
-    return tf.nn.relu(hidden + x)
+    return swish(hidden + x)
+
 
 def preprocess(x):
     return x - tf.reduce_mean(x, axis=1, keepdims=True)
+
+
+def swish(x):
+    return tf.multiply(x, tf.nn.sigmoid(x))
+
+
+def group_norm(x, group=7, epsilon=1e-5):
+    """
+    https://arxiv.org/abs/1803.08494
+    https://github.com/ppwwyyxx/tensorpack/blob/master/examples/ImageNetModels/vgg16.py
+    """
+    shape = x.get_shape().as_list()
+    ndims = len(shape)
+    assert ndims in [2, 4]
+    chan = shape[1]
+    assert chan % group == 0, chan
+    group_size = chan // group
+
+    orig_shape = tf.shape(x)
+    h, w = orig_shape[2], orig_shape[3]
+
+    x = tf.reshape(x, tf.stack([-1, group, group_size, h, w]))
+
+    mean, var = tf.nn.moments(x, [2, 3, 4], keep_dims=True)
+
+    new_shape = [1, group, group_size, 1, 1]
+
+    beta = tf.get_variable('beta', [chan], initializer=tf.constant_initializer())
+    beta = tf.reshape(beta, new_shape)
+
+    gamma = tf.get_variable('gamma', [chan], initializer=tf.constant_initializer(1.0))
+    gamma = tf.reshape(gamma, new_shape)
+
+    out = tf.nn.batch_normalization(x, mean, var, beta, gamma, epsilon, name='output')
+    return tf.reshape(out, orig_shape, name='output')
